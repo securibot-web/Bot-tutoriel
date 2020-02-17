db447dc87cc037489af678b30411ed8ba27b08ea
Joseph <joseph300905@gmail.com>
2020-02-17 17:17:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 
 bot.on('ready', async () =>{
+  console.log('Le bot est lancé.');
   bot.user.setActivity('Bot tutoriel. Bonjour!');
 });
 
