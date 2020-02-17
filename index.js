44d8f51703baf3abf1c34e0b9d78eedce7d77316
Joseph <joseph300905@gmail.com>
2020-02-17 17:14:21 +0100

    Fix index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 
-bot.on('ready', asycn () =>{
+bot.on('ready', async () =>{
   bot.user.setActivity('Bot tutoriel. Bonjour!');
 });
 
