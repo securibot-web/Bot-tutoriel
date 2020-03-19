01f3f9d355de31e46ab0331a7850fff2408a8eac
Joseph <joseph300905@gmail.com>
2020-03-19 11:23:29 +0100

    Listens for messages

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
+const config = require('./config');
 
-bot.on('ready', async () =>{
-  console.log('Le bot est lancé.');
-  bot.user.setActivity('Bot tutoriel. Bonjour!');
+bot.on('ready', async () => {
+    console.log('Le bot est lancé.');
+    bot.user.setActivity('Bot tutoriel. Bonjour!');
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.on('message', async (msg) => {
+    if(msg.content == 'bonjour'){
+        msg.channel.send('bonjour!')
+    }
+})
+
+bot.login(config.token);
