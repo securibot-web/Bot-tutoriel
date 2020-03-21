44c4620c3100a6669f8f787e0c1fcf31734852c8
Joseph <joseph300905@gmail.com>
2020-03-21 16:12:28 +0100

    Create and implement command handler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 const config = require('./config.js');
+const commands = require('./commands.js')
 
 bot.on('ready', async () => {
     console.log('Le bot est lancé.');
@@ -9,21 +10,12 @@ bot.on('ready', async () => {
 
 bot.on('message', async (msg) => {
     if(msg.content.startsWith(config.prefix) && !msg.author.bot){
-        cmdArray = msg.content.substring(1).split(" ")
-        cmd = cmdArray[0]
-        args = cmdArray.slice(1)
+        cmdArray = msg.content.substring(config.prefix.length).split(" ");
+        cmd = cmdArray[0];
+        args = cmdArray.slice(1);
 
-        if(cmd == 'ping'){
-            msg.channel.send("pong! :joy:")
-        }
-        if(cmd == 'message'){
-            if(args.length < 1) return msg.channel.send("Pas assez d'arguments.")
-            var message = ""
-            for(let i = 0; i < args.length; i++){
-                message += args[i] + " "
-            }
-            msg.channel.send("message: " + message)
-        }
+        let command = commands.getCommand(cmd);
+        if (command) command.run(bot, message, args);
     }
 });
 
