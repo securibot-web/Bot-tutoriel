a1f47b9dcb5083479f9443937abfaaa836ce1ffb
Joseph <joseph300905@gmail.com>
2020-03-21 16:12:46 +0100

    Move message command to commands folder

---
+++ commands/message.js
@@ -0,0 +1,12 @@
+module.exports.run = async (bot, message, args) => {
+    if(args.length < 1) return msg.channel.send("Pas assez d'arguments.")
+    var message = ""
+    for(let i = 0; i < args.length; i++){
+        message += args[i] + " "
+    }
+    msg.channel.send("message: " + message)
+}
+
+module.exports.help = {
+    name: "message"
+}
