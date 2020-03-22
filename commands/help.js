2f15d5fb4ba359d0c326add1d030efef882fecd1
Joseph <joseph300905@gmail.com>
2020-03-22 13:32:41 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
     const re = /"(.*?)"/g;
     const result = [];
     let current;
@@ -8,15 +10,14 @@ module.exports.run = async (bot, msg, args) => {
         result.push(current.pop());
     }
 
-    for(let i = 0; i < result.length; i++){
-        console.log(result[i]);
-    }
+    if(result.length > letters.length) return message.channel.send('Il y a trop de propositions!');
 
     let hEmbed = new Discord.RichEmbed();
     hEmbed.setColor('67b58e');
-    hEmbed.setTitle('Hey test');
-    hEmbed.addField('Just testing');
-    hEmbed.addField('No more');
+    hEmbed.setTitle(`Sondage de ${msg.author.name}`);
+    for(let i = 0; i < result.length; i++){
+        hEmbed.addField(`Choix :regional_indicator_${letters[i]}:`, result[i]);
+    }
     msg.channel.send(hEmbed);
 }
 
