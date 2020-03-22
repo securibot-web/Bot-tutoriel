1791266cfb87e778d2efcc7b439a1ba629ca18da
Joseph <joseph300905@gmail.com>
2020-03-22 13:13:32 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
     let hEmbed = new Discord.RichEmbed();
-    hEmbed.setColor('67b58e');
+    hEmbed.setColor('#67b58e');
     hEmbed.setTitle('Hey test');
     hEmbed.addField('Just testing');
     hEmbed.addField('No more');
