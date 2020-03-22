573002b3d6bbd4739e5b97211b659a9b852c1e68
Joseph <joseph300905@gmail.com>
2020-03-22 14:23:29 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
-    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const letters = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
 
     const re = /"(.*?)"/g;
     const result = [];
