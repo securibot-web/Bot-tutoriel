117e269e7fe0f7960675437994efde3017f7df83
Joseph <joseph300905@gmail.com>
2020-03-22 13:10:47 +0100

    Fix help.js

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
-    let hEmbed = new Discord.RichEmbed()
-    .setColor('67b58e')
-    .setTitle('Hey test')
-    .addTitle('Just testing')
-    .addTitle('No more');
+    let hEmbed = new Discord.RichEmbed();
+    hEmbed.setColor('67b58e');
+    hEmbed.setTitle('Hey test');
+    hEmbed.addField('Just testing');
+    hEmbed.addField('No more');
     msg.channel.send(hEmbed);
 }
 
