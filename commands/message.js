3c134f34cefc6ae14417cd6f513233d236d68870
Joseph <joseph300905@gmail.com>
2020-03-29 19:59:44 +0200

    Optimise message.js

--- commands/message.js
+++ commands/message.js
@@ -1,9 +1,6 @@
 module.exports.run = async (bot, msg, args) => {
     if(args.length < 1) return msg.channel.send("Pas assez d'arguments.")
-    var message = ""
-    for(let i = 0; i < args.length; i++){
-        message += args[i] + " "
-    }
+    var message = args.join(' ');
     msg.channel.send("message: " + message)
 }
 
