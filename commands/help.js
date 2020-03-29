6899dede52d7c9934fc3a8a799368af40c2992d3
Joseph <joseph300905@gmail.com>
2020-03-29 21:56:51 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, msg, args) => {
+    let hEmbed = new Discord.MessageEmbed()
+    .setColor('009999')
+    .setTitle('Aide de Bot Tutoriel')
+    .setDescription('Tout ce que vous avez besoin de savoir sur Bot Tutoriel')
+    .addField('Commandes:', '- message: Répéter un message choisi.')
+    .addField('Préfixe:', 'Utilisez le préfixe ! avant chaque commande de Bot Tutoriel')
+    .setFooter("J'espère que ça vous sera utile!");
+    msg.channel.send(hEmbed);
+}
+
+module.exports.help = {
+    name: 'help'
+}
