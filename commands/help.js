3615ee317857c59634e1c11d409a18f85ae1f8d8
Joseph <joseph300905@gmail.com>
2020-03-29 20:04:15 +0200

    Fix help.js

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,13 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, msg, args) => {
-    const hEmbed = new Discord.MessageEmbed()
+    let hEmbed = new Discord.MessageEmbed()
     .setColor('1b65b5')
     .setTitle('Bot Tutoriel')
     .setDescription("L'aide de Bot Tutoriel")
     .addField('Commandes:',
     '- message: Répéter le message que vous avez entré.' +
-    '- ping: Répond par "pong".')
-    .setAuthor('Bot Tutoriel')
-    .setFooter("J'espère que cette commande vous a aidé!")
-    .setTimestamp();
-
+    '- ping: Répond "pong".');
     msg.channel.send(hEmbed);
 }
 
