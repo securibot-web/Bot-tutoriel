dbdb5a3e5696c926104651d6a9a1ecf19e729cc9
Joseph <joseph300905@gmail.com>
2020-03-30 17:32:04 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, msg, args) => {
     const awnser = ["Oui, c'est sûr!", "Hmmmm je ne sais pas...", "Non, je suis sûr que non!"];
-    msg.channel.send(awnser[Math.floor(Math.random() * (awnser.length - 1))]);
+    msg.channel.send(awnser[Math.floor(Math.random() * awnser.length)]);
 }
 
 module.exports.help = {
