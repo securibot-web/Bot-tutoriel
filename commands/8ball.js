a5c0483443a3a2b8172896818a1bfc7cd2f33430
Joseph <joseph300905@gmail.com>
2020-03-30 17:20:43 +0200

    Create 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,8 @@
+module.exports.run = async (bot, msg, args) => {
+    const awnser = ["Oui, c'est sûr!", 'Peut-être, je sais pas moi!', 'Non, je ne pense pas du tout!'];
+    msg.channel.send(awnser[Math.random() * awnser.length]);
+}
+
+module.exports.help = {
+    name: '8ball'
+}
