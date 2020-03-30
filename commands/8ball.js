ce7e17539b61fa78faf1b59143000f985684402f
Joseph <joseph300905@gmail.com>
2020-03-30 17:24:17 +0200

    Delete 8ball.js

--- commands/8ball.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = async (bot, msg, args) => {
-    const awnser = ["Oui, c'est sûr!", 'Peut-être, je sais pas moi!', 'Non, je ne pense pas du tout!'];
-    msg.channel.send(awnser[Math.floor(Math.random() * awnser.length]));
-}
-
-module.exports.help = {
-    name: '8ball'
-}
