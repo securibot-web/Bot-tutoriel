b5fc464a3568a2b352ae07d403b39140de813f25
Joseph <joseph300905@gmail.com>
2020-03-31 22:08:19 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,10 +1,23 @@
+const Discord = require('discord.js');
+
 module.exports.run = async (bot, msg, args) => {
     if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.channel.send("Vous n'avez pas le droit de kick des membres.");
     if(!msg.guild.me.hasPermission('KICK_MEMBERS')) return msg.channel.send("Je n'ai pas le droit de kick des membres");
-    if(args.length > 1) return msg.channel.send("Trop d'arguments");
+    if(args.length < 1) return msg.channel.send("Pas assez d'arguments");
     const member = msg.mentions.members.first();
+    const reason = args.shift().join(" ");
     if(!member) return msg.channel.send("Le membre est invalide");
     member.kick();
+
+    let hEmbed = new Discord.MessageEmbed()
+    .setColor('#00e68a')
+    .setTitle('Rapport de Kick')
+    .setDescription(`Un kick a été effctué par ${msg.author}`)
+    .addFields(
+        { name: 'Auteur:', value: msg.author },
+        { name: 'Membre:', value: member },
+        { name: 'Raison:', value: reason}
+    )
 }
 
 module.exports.help = {
